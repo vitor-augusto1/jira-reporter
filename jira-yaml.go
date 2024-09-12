59591b6a26ca971a43fe8b5b2089d54a24b116c7
vitor-augusto1 <v1t0r4ug@gmail.com>
2024-09-11 22:46:12 -0300

    feat: Map and parse the yaml configuration schema file

--- jira-yaml.go
+++ jira-yaml.go
@@ -6,3 +6,34 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+type JiraProjectMainConfig struct {
+	Name    string `yaml:"name"`
+	BaseUrl string `yaml:"baseUrl"`
+}
+
+type JiraProjectConfig struct {
+	Project JiraProjectMainConfig `yaml:"project"`
+}
+
+type IssueTypeKeywords map[string]struct {
+	IssueType string `yaml:"issueType"`
+}
+
+type JiraProjectYamlConfig struct {
+	Jira     JiraProjectConfig `yaml:"jira"`
+	Dirs     string            `yaml:"dirs"`
+	Keywords IssueTypeKeywords `yaml:"keywords"`
+}
+
+func parseYamlConfigFile(filePath string) (*JiraProjectYamlConfig, error) {
+	var jpy JiraProjectYamlConfig
+	yamlFile, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	err = yaml.Unmarshal(yamlFile, &jpy)
+	if err != nil {
+		return nil, err
+	}
+	return &jpy, nil
+}
