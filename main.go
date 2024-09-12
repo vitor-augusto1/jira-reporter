0e0c8f98f6c654a7f9b02abe0aa579d73a241bd0
vitor-augusto1 <v1t0r4ug@gmail.com>
2024-09-11 22:47:05 -0300

    feat: Load the configs from the yaml file

--- main.go
+++ main.go
@@ -5,26 +5,29 @@ import (
 	"os"
 )
 
-var (
-	PROJECT_BASE_URL = os.Getenv("PROJECT_BASE_URL")
-)
-
 func main() {
+  parsedJiraConfig, err := parseYamlConfigFile("./test.yaml")
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Error parsing the yaml config file.\n%s\n", err)
+  }
+  fmt.Fprintf(os.Stdout, "%s\n", parsedJiraConfig)
 	creds := NewJiraBasicAuthCreds()
 	creds.username = os.Getenv("PROJECT_USERNAME")
 	creds.password = os.Getenv("PROJECT_PASSWORD")
-	jc := NewJiraClient(creds, PROJECT_BASE_URL)
+	jc := NewJiraClient(creds, parsedJiraConfig.Jira.Project.BaseUrl)
   wsl := Weasel{
     Keywords: []string{"TODO","FIXME","REFACTOR"},
   }
-  fmt.Println(wsl.todoRegex("TODO"))
+  fmt.Fprintf(os.Stdout, "TODO regex: %s\n", wsl.todoRegex("TODO"))
+  // TODO: Implement depth searchs reports. Visit every file in the "dirs" param
+  // In configuration file
   wsl.searchTodos("./test.txt", func(todo Todo) error {
     issue := jc.CreateNewIssueFromTODO(todo)
     if issue != nil {
-      fmt.Printf("Issue to be reported: '%s' \n", issue.Summary)
+      fmt.Fprintf(os.Stdout, "Issue to be reported: '%s'\n", issue.Summary)
       err := jc.ReportIssueAsJiraTicket(issue)
       if err != nil {
-        fmt.Printf("Cant report this following issue: '%s'. Skipping for now.\n", issue.Summary)
+        fmt.Fprintf(os.Stderr, "Cant report this following issue: '%s'. Skipping for now.\n", issue.Summary)
       }
     }
     return nil
