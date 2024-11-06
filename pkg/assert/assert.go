aeab907441727faf79ee38ab266f00bf0f01f99c
vitor-augusto1 <v1t0r4ug@gmail.com>
2024-11-05 22:22:57 -0300

    feat: Add new function to asser nil params

--- pkg/assert/assert.go
+++ pkg/assert/assert.go
@@ -10,33 +10,41 @@ import (
 
 func runAssert(msg string, args ...any) {
 	slogValuesMessages := []any{
-    "Assert message",
+		"Assert message",
 		msg,
 	}
 	slogValuesMessages = append(slogValuesMessages, args...)
 	for i := 0; i < len(slogValuesMessages); i += 2 {
-    fmt.Fprintf(os.Stderr, "  %s: %v\n", slogValuesMessages[i], slogValuesMessages[i+1])
+		fmt.Fprintf(os.Stderr, "  %s: %v\n", slogValuesMessages[i], slogValuesMessages[i+1])
 	}
 	fmt.Fprintln(os.Stderr, string(debug.Stack()))
 	os.Exit(1)
 }
 
 func Assert(truthy bool, msg string, args ...any) {
-  if !truthy {
-    runAssert(msg, args...)
-  }
+	if !truthy {
+		runAssert(msg, args...)
+	}
 }
 
 func NotNil(item any, msg string, args ...any) {
 	if item == nil || reflect.ValueOf(item).Kind() == reflect.Ptr && reflect.ValueOf(item).IsNil() {
 		slog.Error("NotNil#nil value encountered")
-    runAssert(msg, args...)
+		runAssert(msg, args...)
 	}
 }
 
+func Nil(item any, msg string, args ...any) {
+	if item == nil || (reflect.ValueOf(item).Kind() == reflect.Ptr && reflect.ValueOf(item).IsNil()) {
+		return
+	}
+	slog.Error("Nil#not nil value encountered")
+	runAssert(msg, args...)
+}
+
 func NoError(err error, msg string, data ...any) {
-  if err != nil {
-    data = append(data, "error", err)
-    runAssert(msg, data...)
-  }
+	if err != nil {
+		data = append(data, "error", err)
+		runAssert(msg, data...)
+	}
 }
