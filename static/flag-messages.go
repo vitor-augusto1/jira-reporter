563a84eeb695adf6c5cd5f161110f3a3e3692bee
vitor-augusto1 <v1t0r4ug@gmail.com>
2024-11-27 22:13:35 -0300

    feat: Add flag statics messages

---
+++ static/flag-messages.go
@@ -0,0 +1,8 @@
+package static
+
+const (
+	REPORT_MESSAGE string = "Report TODOs in tracked files"
+	PURGE_MESSAGE  string = "Purge all TODOs in tracked files"
+	LIST_MESSAGE   string = "List all TODOs in tracked files"
+	SILENT_MESSAGE string = "Runs jira-weasel without banner and messages"
+)
