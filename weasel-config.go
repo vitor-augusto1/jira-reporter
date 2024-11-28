b253e1c381f940c5056ca2660cc28177cc181c1d
vitor-augusto1 <v1t0r4ug@gmail.com>
2024-11-27 22:19:40 -0300

    feat: Map config toml to struct

---
+++ weasel-config.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/vitor-augusto1/jira-weasel/pkg/logger"
+)
+
+type Project struct {
+	Url string `toml:"url"`
+	Key string `toml:"key"`
+}
+
+type Credentials struct {
+	Username string `toml:"username"`
+	Password string `toml:"password"`
+}
+
+type Config struct {
+	Remote      string            `toml:"remote"`
+	Project     Project           `toml:"project"`
+	Credentials Credentials       `toml:"credentials"`
+	Keywords    map[string]string `toml:"keywords"`
+}
+
