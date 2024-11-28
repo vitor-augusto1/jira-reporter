c19c2c4e1133eccd39aa34ff873f489772d5dc07
vitor-augusto1 <v1t0r4ug@gmail.com>
2024-11-27 22:16:35 -0300

    refactor: Remove unused func

--- issue.go
+++ issue.go
@@ -63,11 +63,3 @@ type IssueType = string
 func NewIssue() *Issue {
 	return &Issue{}
 }
-
-func sliceContainsValidIssuesType(sl []string, validOnes map[string]bool, errorCallback func(invalidType string)) {
-  for _, item := range sl {
-    if !validOnes[item] {
-      errorCallback(item)
-    }
-  }
-}
